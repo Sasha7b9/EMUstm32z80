ffbb3a6292ff8cd448aed7ebf8d931a78d71fa66
Sasha <Sasha7b9@tut.by>
2015-12-26 04:46:12 +0300

    Create c-file from ROM

---
+++ scripts/create_rom.py
@@ -0,0 +1,20 @@
+import struct
+
+file = open("..\\EMUstm32z80\\ROM\\48.ROM", "br")
+fileOut = open("ROM.c", "w")
+
+fileOut.write("uint8 RAM48[65536] =\n")
+fileOut.write("{\n")
+
+for i in range (16384 // 16):
+    fileOut.write("   ")
+    for j in range (16):
+        value = struct.unpack('@B', file.read(1))
+        fileOut.write("  " + str(value[0]) + ",")
+    fileOut.write("\n")
+
+fileOut.write("};\n")
+    
+file.close()
+fileOut.close()
+
